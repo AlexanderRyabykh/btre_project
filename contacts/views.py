d0f52e9aa24ea65fa57c60e96b400e1232e5b03e
Alexander Ryabykh <sanchitos@bk.ru>
2021-03-11 14:43:11 +0300

    Multiple inquiries for the given listing and authorized user are restricted

--- contacts/views.py
+++ contacts/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from .models import Contact
 
 
-
 def contact(request):
     if request.method == 'POST':
         listing_id = request.POST['listing_id']
@@ -15,6 +14,17 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+
+        # Check if user has made inquiry already
+        if request.user.is_authenticated:
+            print(request.user.is_authenticated)
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                print(has_contacted)
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/' + listing_id)
+
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
